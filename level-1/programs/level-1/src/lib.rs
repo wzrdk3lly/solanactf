58e411dbeda8802ccca3d1b6d63a02c20bb8d591
wzrdk3lly <dkmk1149@gmail.com>
2022-05-04 14:25:25 -0400

    create program + baseline test script

--- level-1/programs/level-1/src/lib.rs
+++ level-1/programs/level-1/src/lib.rs
@@ -4,12 +4,67 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 
 #[program]
 pub mod level_1 {
+
     use super::*;
 
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+       
+        let account = &mut ctx.accounts.my_account_buffer;
+        
+        account.data = 0;      
+        
+        Ok(())
+    }
+
+    pub fn increment_by_ten(ctx: Context<IncrementByTen>) -> Result<()>{
+        
+        let account = &mut ctx.accounts.my_account_buffer;
+
+        account.data += 10;
+        
+        Ok(())
+    }
+    
+
+    pub fn set_data(ctx: Context<SetData>, data: u8) -> Result<()>{
+
+        let account = &mut ctx.accounts.my_account_buffer;
+
+        account.data = data;
+
         Ok(())
     }
 }
 
+
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+#[account(init, payer = authority, space = 48)] 
+pub my_account_buffer: Account<'info, MyAccountBuffer>,
+#[account(mut)]
+pub authority: Signer<'info>,
+pub system_program: Program<'info, System>
+}
+
+#[derive(Accounts)]
+pub struct IncrementByTen<'info> {
+    
+    #[account(mut)]
+    pub my_account_buffer: Account<'info, MyAccountBuffer>,
+    pub authority: Signer<'info>,
+}
+
+#[derive(Accounts)]
+pub struct SetData<'info>{
+#[account(mut)]
+pub my_account_buffer: Account<'info, MyAccountBuffer>,
+/// CHECK: using AccountInfo
+pub authority: AccountInfo<'info>
+}
+
+#[account]
+pub struct MyAccountBuffer{
+    pub authority: Pubkey,
+    pub data: u8,
+}
+
--- level-1/tests/level-1.ts
+++ level-1/tests/level-1.ts
@@ -2,15 +2,28 @@ import * as anchor from "@project-serum/anchor";
 import { Program } from "@project-serum/anchor";
 import { Level1 } from "../target/types/level_1";
 
+
 describe("level-1", () => {
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  anchor.setProvider(anchor.Provider.env());
 
   const program = anchor.workspace.Level1 as Program<Level1>;
+  const increment = anchor.web3.Keypair.generate();
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+  it("Initialize the data", async () => {
+    // Use this
+    const tx = await program.rpc.initialize({
+      accounts:{
+        myAccountBuffer: increment.publicKey,
+        authority: program.provider.wallet.publicKey,
+        systemProgram: anchor.web3.SystemProgram.programId
+      },
+      signers:[increment]
+      
+    });
+    let incrementAccount = await program.account.myAccountBuffer.fetch(increment.publicKey)
+    console.log("initialize the account --> ", incrementAccount)
   });
+
 });
+
